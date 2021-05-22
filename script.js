b6c2678ec348dff891333271f7c62021e93601e6
Keely Bailey <keelybailey3@gmail.com>
2021-05-21 22:01:15 -0500

    Add random character generator functions to js

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-//Generator functions
+//testing
+console.log("is this thing on")
 
-function getRandomLower (){
-    return String.fromCharCode()
+//What is the object?
+/* The final object/product is the generated password.
+
+Password Criteria:
+ - lowercase
+ - uppercase
+ - numeric
+ - special characters
+ - number of characters -- typed in
+
+STEPS:
+ 1. User lands on the page and clicks button to generate a password... EVENT LISTENER
+ 2. Selectors appear for password criteria... IF ELSE
+ 3. Click button to get password... another event listener?
+ 4. Display password on page
+
+ */
+
+ //Starting with generating the random characters
+
+ function getRandomLowerLetter(){
+    return String.fromCharCode(Math.floor(Math.random()*26)+97);
 }
+/*string. returns a string rather than other data types;
+    floor provides a whole number; random provides random number up to 26, letters in the alphabet
+    +97 because that is where the lowercase letters start in the character code*/
+
+function getRandomUpperLetter(){
+    return String.fromCharCode(Math.floor(Math.random()*26)+65)
+}
+
+function getRandomNumber(){
+    return String.fromCharCode(Math.floor(Math.random()*10)+48)
+}
+
+/*function getRandomSpecialCharacter(){
+
+}*/
