3afb4965a48234eec28df353651e9e49edbebd8b
Keely Bailey <keelybailey3@gmail.com>
2021-07-26 14:26:27 -0500

    Submit for grade

--- README.md
+++ README.md
@@ -1,23 +1,8 @@
 # password-generator-homework
 
-## User Story<br>
-AS AN employee with access to sensitive data<br>
-I WANT to randomly generate a password that meets certain criteria<br>
-SO THAT I can create a strong password that provides greater security<br>
+Description: Landing page provides user a randomly generated password given requirements
 
-## Acceptance Criteria<br>
-GIVEN I need a new, secure password<br>
-WHEN I click the button to generate a password<br>
-THEN I am presented with a series of prompts for password criteria<br>
-WHEN prompted for password criteria<br>
-THEN I select which criteria to include in the password<br>
-WHEN prompted for the length of the password<br>
-THEN I choose a length of at least 8 characters and no more than 128 characters<br>
-WHEN prompted for character types to include in the password<br>
-THEN I choose lowercase, uppercase, numeric, and/or special characters<br>
-WHEN I answer each prompt<br>
-THEN my input should be validated and at least one character type should be selected<br>
-WHEN all prompts are answered<br>
-THEN a password is generated that matches the selected criteria<br>
-WHEN the password is generated<br>
-THEN the password is either displayed in an alert or written to the page<br>
+Screenshot: (./Code/Homework/03-Homework-Password-Generator/password-generator/Capture.JPG)<br>
+
+Live Link: https://spacerambler.github.io/password-generator-homework/
+Github Link: https://github.com/spacerambler/password-generator-homework
--- script.js
+++ script.js
@@ -30,40 +30,40 @@ STEPS BRAINSTORM:
  var numbersEl = document.getElementById("numbers");
  var symbolsEl = document.getElementById("symbols");
  var resultEl = document.getElementById("password-container")
- 
+
   function getRandomLowerLetter(){
      return String.fromCharCode(Math.floor(Math.random()*26)+97);
  }
  /*string. returns a string rather than other data types;
      floor provides a whole number; random provides random number up to 26, letters in the alphabet
      +97 because that is where the lowercase letters start in the character code*/
- 
+
  function getRandomUpperLetter(){
      return String.fromCharCode(Math.floor(Math.random()*26)+65)
  }
- 
+
  function getRandomNumber(){
      return String.fromCharCode(Math.floor(Math.random()*10)+48)
  }
- 
+
  function getRandomSpecialCharacter(){
      var symbols = "!@#$%^&*?><"
      return symbols[Math.floor(Math.random() * symbols.length)]
  }
  //TESTING FUNCTIONS
- console.log(getRandomLowerLetter());
- console.log(getRandomUpperLetter());
- console.log(getRandomNumber());
- console.log(getRandomSpecialCharacter());
+//  console.log(getRandomLowerLetter());
+//  console.log(getRandomUpperLetter());
+//  console.log(getRandomNumber());
+//  console.log(getRandomSpecialCharacter());
 
- 
+
  var randomFunctions = {
      lower: getRandomLowerLetter,
      upper: getRandomUpperLetter,
      number: getRandomNumber,
      specialCharacter: getRandomSpecialCharacter,
  }
- 
+
  /* Add event listener */
    button.addEventListener("click", function() {
      var length = +lengthEl.value;
@@ -71,40 +71,50 @@ STEPS BRAINSTORM:
      var hasUpper = upperEl.checked;
      var hasNumbers = numbersEl.checked;
      var hasSymbols = symbolsEl.checked;
- 
-     console.log(length, hasLower, hasUpper, hasNumbers, hasSymbols);
-     //resultEl.appendChild.innertext = generatePassword(length, hasLower, hasUpper, hasNumbers, hasSymbols);
+
+    //  console.log(length, hasLower, hasUpper, hasNumbers, hasSymbols);
+    resultEl.innertext = generatePassword(length, hasLower, hasUpper, hasNumbers, hasSymbols);
 
  });
- 
+
  /*Password function*/
  function generatePassword(hasLower, hasUpper, hasNumbers, hasSymbols, length){
- 
+
      let generatedPassword = "";
- 
+
      var typesCount = hasLower + hasUpper + hasNumbers + hasSymbols;
         //this is showing what has been checked
 
-     console.log("typesCount:" + typesCount);
+    //  console.log("typesCount:" + typesCount);
 
      //Add in an array here using object literals aka curly brackets around types which will be used in the loop
      //also need to add in a filter on the var for the items... if they aren't selected then they won't show in the array
      //filter creates a new array with all the elements that pass through the function
 
      var typesArray = [{hasLower}, {hasUpper}, {hasNumbers}, {hasSymbols}].filter(item => Object.values(item)[0])
-        //in filter... object/valuse/item are all pre-prescribed syntax within the array
+        //in filter... object/valuee/item are all pre-prescribed syntax within the array
 
 
      //For each loop using types array, types count is the increment
-     //need to create a new var in the loop 
+     //need to create a new var in the loop
+    for (let i = 0; i < length; i += typesCount) {
+        //+= adds a type to be incorporated
+     typesArray.forEach(item => {
+        var funcName = Object.keys(type)[0];
+        //returns an array of a given object's own property names
+        // console.log('funcName: ' + funcName)
+        generatePassword += randomFunctions[funcName]();
+    });
+    }
+    console.log(generatedPassword.slice(0, length))
 
+    const finalPassword = generatedPassword.slice(0, length);
 
-     //append the generatedPassword created in this function to randomize 
+    console.log(finalPassword)
+
+	return finalPassword;
  }
-
+generatePassword()
 /*Input the generatedPassword string into the HTML*/
 
-
-//NICE TO HAVES... come back after assignment is turned in 
-//Add validation for the length requirements
-//
+//Followed along the best I could with a video from Traversy Media https://www.youtube.com/watch?v=duNmhKgtcsI&t=1817s
